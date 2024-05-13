6a8c9a6ed0cdd4baf0045aa4f55d0c8815aef39d
harsh jaiswal <tech.harsh2003@gmail.com>
2024-05-13 23:19:01 +0530

    Pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Signout from "./pages/Signout";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 
 export default function App() {
-  return (
-    <h1 className="text-red-500">Hello</h1>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-out" element={<Signout />} />
+                <Route path="/sign-up" element={<Signup />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
