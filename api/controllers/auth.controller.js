3c9ee35bede9d19dd5e013e63b69a4cb665c485c
harsh jaiswal <tech.harsh2003@gmail.com>
2024-05-15 08:41:05 +0530

    Creating middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    const newUser = new User({ username: username, email: email, password: password });
-    await newUser.save();
-    res.status(201).json("User created successfully");
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username: username, email: email, password: hashedPassword });
+    console.log(newUser);
+    try {
+        await newUser.save();
+        res.status(201).json("User created successfully");
+    } catch (error) {
+        next(error);
+    }
 }
