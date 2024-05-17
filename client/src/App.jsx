633e296236f996b6d0590c87c0eb56497e75f005
harsh jaiswal <tech.harsh2003@gmail.com>
2024-05-17 20:06:57 +0530

    Image update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signout from "./pages/Signout";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 export default function App() {
@@ -19,7 +20,9 @@ export default function App() {
                 <Route path="/sign-up" element={<Signup />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/profile" element={<Profile />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path="/profile" element={<Profile />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
