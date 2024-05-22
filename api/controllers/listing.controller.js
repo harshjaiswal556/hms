80684cfa1e3ddde09c2e595d3b8306633580d988
harsh jaiswal <tech.harsh2003@gmail.com>
2024-05-22 11:47:10 +0530

    Image slider

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,33 @@ export const deleteListing = async (req, res, next) => {
 
     }
 }
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if (req.user.id !== listing.useRef) {
+        return next(errorHandler(401, 'You can only update your own listing!'));
+    }
+    try {
+        const updateListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+        return res.status(200).json(updateListing);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+        return res.status(200).json(listing);
+    } catch (error) {
+        next(error);
+    }
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 export default function App() {
     return (
@@ -20,9 +22,11 @@ export default function App() {
                 <Route path="/sign-up" element={<Signup />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/listing/:listingId" element={<Listing />} />
                 <Route element={<PrivateRoute />}>
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/create-listing" element={<CreateListing />} />
+                    <Route path="/update-listing/:listingId" element={<UpdateListing />} />
                 </Route>
             </Routes>
         </BrowserRouter>
